9f68d5b57bb15bc21d62413fd0ba7d9735b9d01f
Brandon <leevash203@gmail.com>
2022-07-15 13:47:20 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,22 +1,34 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_
+#define _MAIN_
 
-#include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
 
-int _putchar(char c);
+/**
+* struct op - flag / function object
+* @c: flag
+* @f: function
+*/
+
+typedef struct op
+{
+	char *c;
+	int (*f)(va_list);
+} op_t;
+
 int _printf(const char *format, ...);
-int print_num(long int n);
-int print_string(char *str);
-int print_binary(unsigned int num);
-int print_unknown_spec(char c);
-int print_odh(char c, unsigned int num);
-int dec_to_oct(unsigned int num);
-int dec_to_hex(char c, unsigned int num);
-int print_S(char *);
-int print_reverse(char *s);
-int print_rot13(char *c);
-int print_pointer(void *p);
+int _putchar(char c);
+int print_c(va_list ar_list);
+int (*get_func(char s))(va_list ar_list);
+int print_s(va_list ar_list);
+int print_d(va_list ar_list);
+int print_i(va_list ar_numlist);
+int print_b(va_list binary_list);
+int print_u(va_list ar_list);
+int print_o(va_list ar_list);
+int print_x(va_list ar_list);
+int print_X(va_list ar_list);
 
 #endif
